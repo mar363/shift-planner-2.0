228b51744a05ab5fe88372f319fdf7b729109afa
Cristina Zaharia <ncristinanedea@gmail.com>
2025-09-01 19:42:32 +0300

    Initial commit: Spring Boot project setup with PstgresSql config

---
+++ src/main/java/com/shiftplanner/shift_planner/ShiftPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.shiftplanner.shift_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiftplanner/shift_planner/ShiftPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftplanner.shift_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
